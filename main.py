b7deed504ff67e71f670819bdb9c1d87adb647e7
zarinasaliakhova <zarinarus@yandex.ru>
2024-07-27 17:09:47 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+print("Hello world")
+
+print(9**0.5*5)
+
+print(9.99>9.98 and 1000!=1000.1)
+
+name1 = '1234'
+name2 = '5678'
+print(int(name1[1:3]) + (int(name2[1:3])))
+
+name1= '13.42'
+name2= '42.13'
+print(int(name1[0:2]) == float(name2[3:]) or float(name1[3:]) == int(name2[0:2]))
